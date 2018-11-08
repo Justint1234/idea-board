ca3590167b2460e054a092ef30778a2699330f54
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-11-08 11:13:30 -0500

    second push

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use('/', routes)
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
 
-const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`)
 })
