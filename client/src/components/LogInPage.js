a5b1aa26a7c8c7a39768efaf343ecece0414ae6f
king0120 <king0120@gmail.com>
2018-11-09 09:40:27 -0500

    can create a new user

--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -6,7 +6,32 @@ import axios from 'axios'
 
 class LogInPage extends Component {
   state = {
-    users: []
+    users: [],
+    newUser: {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event) => {
+    console.log('name', event.target.name)
+    console.log('value', event.target.value)
+    const updatedNewUser = {...this.state.newUser}
+
+    // Event Target Name will be either 'username' or 'password'
+    updatedNewUser[event.target.name] = event.target.value
+    this.setState({newUser: updatedNewUser})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    // Make post to our api to create new user
+    axios.post('/api/users', this.state.newUser).then(res => {
+      // when we get that data back, we need to navigate to the new users page
+      console.log(res.data)
+    })
+    
   }
 
   getAllUsers = () => {
@@ -29,6 +54,19 @@ class LogInPage extends Component {
             {user.username}
           </div>
         )) }
+
+        <h3>Sign-Up</h3>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="username">User Name: </label>
+            <input onChange={this.handleChange} value={this.state.newUser.username} type="text" name="username"/>
+          </div>
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input onChange={this.handleChange} value={this.state.newUser.password} type="password" name="password"/>
+          </div>
+          <button type="submit">Create User</button>
+        </form>
       </div>
     );
   }
